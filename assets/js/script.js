f9e0ce15bdf07c7862bd45cbcc3d794a8679296c
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-08-19 18:19:52 +0000

    Complete  JS code for Arabic->Roman function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-console.log(`Hello there`);
 
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+  });
 
 
 function handleForm(event) {
@@ -14,18 +16,46 @@ function handleForm(event) {
     result.innerText = userOutput;
 }
 
-
+/**
+ * This function returns the Roman numeral equivalent of a 
+ * number in Arabic notation, up to 3999
+ * input = reversed string of the number submitted by user
+ */
 function returnRoman(input) {
-    let userOutput = "SPQR";
+    let userOutput = "";
     let myString = input.toString(10);
     let myLength = myString.length;
-    // console.log(myLength);
 
-    for (let i=myLength; i>0; i--){
-        console.log(i);
+    let myGnirts = myString.split('').reverse().join('');
+    console.log(myGnirts);
 
+    for (let i = 0; i <myLength; i++) {
+        // console.log("my i");
+        // console.log(i);
+        userOutput = returnRomanStep(i, myGnirts[i]) + userOutput;
+        // console.log(userOutput);
     }
-    console.log("Done");
-
+    
     return userOutput;
+}
+/**
+ * This function uses the place value to return 
+ * the Roman numeral equivalent
+ * place = place value considered (from units to thousands)
+ * digit = original number
+ */
+function returnRomanStep(place, digit) {
+    const myOnes = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    const myTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const myHuns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const myThou = ["", "M", "MM", "MMM" ];
+
+    if (place === 0) {addPlaceValue = myOnes[digit]; console.log(addPlaceValue);}
+    else if (place === 1) {addPlaceValue = myTens[digit]; console.log(addPlaceValue);}
+    else if (place === 2) {addPlaceValue = myHuns[digit]; console.log(addPlaceValue);}
+    else if (place === 3) {addPlaceValue = myThou[digit]; console.log(addPlaceValue);}
+
+    console.log(addPlaceValue);
+
+    return addPlaceValue;
 }
